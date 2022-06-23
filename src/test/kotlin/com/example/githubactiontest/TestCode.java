ce6828dc1650418f2c14e3176cfaf370edc0e94a
Kimakjun <dkd7142@naver.com>
2022-06-24 01:28:08 +0900

    add test code

---
+++ src/test/kotlin/com/example/githubactiontest/TestCode.java
@@ -0,0 +1,13 @@
+package com.example.githubactiontest;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestCode {
+
+    @Test
+    public void test(){
+        System.out.println("test code");
+    }
+
+}
